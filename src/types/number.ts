fd297b4cb6550552a511ea1129d2e8a38fcd5d4f
zero <tobewhatwewant@gmail.com>
2019-03-14 19:54:34 +0800

    :sparkles:(number.min|max|greater|less): specifies the minimum/maximum/greater/less than specified value

--- src/types/number.ts
+++ src/types/number.ts
@@ -14,4 +14,36 @@ export default class extends Type<number> {
     this.addValidator(assert(number, '{path} should be number'));
     return this;
   }
+
+  public min(limit: number) {
+    this.addValidator(assert(
+      (v: number) => v >= limit,
+      `{path} with value "{value}" must be greater than or equal to ${limit}`,
+    ));
+    return this;
+  }
+
+   public max(limit: number) {
+    this.addValidator(assert(
+      (v: number) => v <= limit,
+      `{path} with value "{value}" must be less than or equal to ${limit}`,
+    ));
+    return this;
+  }
+
+  public greater(limit: number) {
+    this.addValidator(assert(
+      (v: number) => v > limit,
+      `{path} with value "{value}" must be greater than ${limit}`,
+    ));
+    return this;
+  }
+
+   public less(limit: number) {
+    this.addValidator(assert(
+      (v: number) => v < limit,
+      `{path} with value "{value}" must be less than ${limit}`,
+    ));
+    return this;
+  }
 }
